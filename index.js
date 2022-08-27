89f80b0b5565701449423ef33e26f4faaca4a93f
kobe koto <kobe_koto@outlook.com>
2022-08-27 16:52:05 +0800

    Update
    · 添加BlockIP和BlockRegion功能。
    · EnableCache現在是DisableCache。
    · LangCode現在重構為RegionCode。
    · 各種提示的Header統一到UniHeader内。
    · 添加一點點注釋

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const DomainReplaceKey = ["anti-fw","anti-fw-cf","auto-proxy-test","auto-proxy"]
 const AllowList = undefined;
 const BlockList = ["114514.jp","114514.cn"];
 
+//設置屏蔽的國家或地區，示例中的國家和地區代碼"xx"和"xx"目前并不存在于地球這個行星上(必須是小寫，如果你的星球有的話請發issue告訴我一下)，不啓用請設置為false(Boolean值)。
+const BlockRegion = ["xy","xx"];
+
+//設置屏蔽的IP位址，不啓用請設置為false(Boolean值)。
+const BlockIP = ["192.168.254.78","192.168.254.87"];
+
 //是否在請求被block的時候展示 可用/阻止 域名，Boolean類型，允許true和false。
 const ShowAvailableList = true;
 
 //設定代理的網址使用的protocol。支持 "http" 和 "https" 。
 const URLProtocol = "http";
 
-//選擇是否强制啓用緩存（需要瀏覽器支援），允許true和false （Boolean 值）。
-const EnableCache = true;
+//選擇是否强制禁用緩存（需要瀏覽器支援），允許true和false （Boolean 值）。
+const DisableCache = true;
 
 //定義 i18n 字串。
 const i18n = {
@@ -29,7 +35,9 @@ const i18n = {
         "DomainNotAllow": "域名不在 AllowList 内. \r\n\r\n",
         "BlockList": "阻止的域名: \r\n",
         "AllowList": "允許的域名: \r\n",
-        "ConfError": "配置錯誤. AllowList 和 BlockList 不能在同一時間被配置. \r\n"
+        "ConfError": "配置錯誤. AllowList 和 BlockList 不能在同一時間被配置. \r\n",
+        "RegionBlocked": "您所在的地區已經被此站點屏蔽. ",
+        "IPBlocked": "您的 IP 位址已經被此站點屏蔽. "
     },
     "en": {
         "WorkersDevNotSupport": "!!!!!! Auto-Proxy does not support \" *.workers.dev \" Subdomain now. !!!!!!\r\n",
@@ -44,7 +52,9 @@ const i18n = {
         "DomainNotAllow": "Domain isn't in AllowList. \r\n\r\n",
         "BlockList": "Block List: \r\n",
         "AllowList": "Allow List: \r\n",
-        "ConfError": "Configuration error. AllowList and BlockList cannot be configured at the same time.\r\n \r\n"
+        "ConfError": "Configuration error. AllowList and BlockList cannot be configured at the same time.\r\n \r\n",
+        "RegionBlocked": "Your region has been blocked by this site. ",
+        "IPBlocked": "Your IP address has been blocked by this site. "
     },
     "jp": {
         "WorkersDevNotSupport": "!!!!!!! 現在、Auto-Proxy は *.workers.dev サブドメインに対応していません. !!!!!!\r\n\r\n",
@@ -59,9 +69,18 @@ const i18n = {
         "DomainNotAllow": "ドメイン名が 「AllowList」 にありません. \r\n\r\n",
         "BlockList": "ブロックされたドメイン: \r\n",
         "AllowList": "許可されたドメイン: \r\n",
-        "ConfError": "設定エラーです. AllowList と BlockList を同時に構成することはできません. \r\n"
+        "ConfError": "設定エラーです. AllowList と BlockList を同時に構成することはできません. \r\n",
+        "RegionBlocked": "お住まいの地域はこのサイトからブロックされています. ",
+        "IPBlocked": "あなたの IP アドレスは、このサイトによってブロックされています. "
     }
 }
+
+//定義UniHeader。。。你一般不需要操心這個東西。
+const UniHeader = {
+    'Content-Type':'application/json;charset=UTF-8',
+    'Access-Control-Allow-Origin':'*',
+    'Cache-Control':'no-store'
+};
 
 addEventListener("fetch", event => {
     event.respondWith(fetchAndApply(event.request));
@@ -69,13 +88,13 @@ addEventListener("fetch", event => {
 
 async function fetchAndApply(request) {
     let i18nLang;
-    let LangCode = request.headers.get('cf-ipcountry');
-    if (LangCode !== null) {
-        LangCode = LangCode.toString().toLowerCase();
+    let RegionCode = request.headers.get('cf-ipcountry');
+    if (RegionCode !== null && RegionCode !== undefined) {
+        RegionCode = RegionCode.toString().toLowerCase();
     }
-    if (LangCode === "cn" || LangCode === "hk" || LangCode === "tw" || LangCode === "mo") {
+    if (RegionCode === "cn" || RegionCode === "hk" || RegionCode === "tw" || RegionCode === "mo") {
         i18nLang = i18n.zh;
-    } else if (LangCode === "jp") {
+    } else if (RegionCode === "jp") {
         i18nLang = i18n.jp;
     } else {
         i18nLang = i18n.en;
@@ -90,9 +109,10 @@ async function fetchAndApply(request) {
         ProxyDomain = ProxyDomain.split("."+DomainReplaceKey[i])[0];
     }
 
-    //替換 "-x-" 為 "."
+    //替換 "-x-" 為 "."。
     ProxyDomain = ProxyDomain.replace(/-x-/gi,".");
 
+    //如果用戶請求了主域名則返回提示。
     let ReturnUsage;
     if (ProxyDomain === "" || ProxyDomain === url.host) {
         if (url.host.slice(-12) === ".workers.dev") {
@@ -107,14 +127,31 @@ async function fetchAndApply(request) {
             i18nLang.Deploy +
             i18nLang.Copyright
             ,{
-                headers: {
-                    'Content-Type':'application/json;charset=UTF-8',
-                    'Access-Control-Allow-Origin':'*',
-                    'Cache-Control':'no-store'
-                }
+                headers: UniHeader
             });
     }
 
+    //檢查用戶是不是在BlockRegion發起的請求。是則返回對應的403頁面。
+    if (!!BlockRegion && BlockRegion.includes(RegionCode)) {
+        return new Response(
+            i18nLang.DomainBlocked +
+            i18nLang.Deploy, {
+            headers: UniHeader,
+            status:403
+        });
+    }
+
+    //檢查用戶的IP是否在BlockIP内。是則返回對應的403頁面。
+    if (RegionCode !== null && !!BlockIP && BlockIP.includes(request.headers.get("cf-connecting-ip"))) {
+        return new Response(
+            i18nLang.IPBlocked +
+            i18nLang.Deploy, {
+            headers: UniHeader,
+            status:403
+        });
+    }
+
+    //檢查用戶請求的ProxyDomain是否在BlockList内或AllowList外，是則返回403頁面。
     if (BlockList !== undefined && AllowList === undefined && BlockList.includes(ProxyDomain)) {
         let BlockListText = i18nLang.BlockList;
         if (ShowAvailableList) {
@@ -131,11 +168,7 @@ async function fetchAndApply(request) {
         }
 
         return new Response(i18nLang.DomainBlocked + BlockListText, {
-            headers:{
-                "Content-Type":"text/plain;charset=UTF-8",
-                'Access-Control-Allow-Origin':'*',
-                'Cache-Control':'no-store'
-            },
+            headers: UniHeader,
             status:403
         });
     } else if (BlockList === undefined && AllowList !== undefined && !AllowList.includes(ProxyDomain)) {
@@ -154,26 +187,19 @@ async function fetchAndApply(request) {
         }
 
         return new Response(i18nLang.DomainNotAllow + AllowListText, {
-            headers:{
-                "Content-Type":"text/plain;charset=UTF-8",
-                'Access-Control-Allow-Origin':'*',
-                'Cache-Control':'no-store'
-            },
+            headers: UniHeader,
             status:403
         });
     } else if ((BlockList !== undefined) && (AllowList !== undefined)){
+        //（靠北哦怎麽會有人這樣子搞的）
         return new Response(i18nLang.ConfError,{
-            headers: {
-                'Content-Type':'application/json;charset=UTF-8',
-                'Access-Control-Allow-Origin':'*',
-                'Cache-Control':'no-store'
-            }
+            headers: UniHeader
         });
     }
 
 
-    //let response = null;
 
+    //上面的全都沒有問題的話就開始請求要訪問的位址了。
     let NewRequestHeaders = new Headers(request.headers);
 
     NewRequestHeaders.set("Host", ProxyDomain);
@@ -192,10 +218,11 @@ async function fetchAndApply(request) {
     let NewResponseHeaders = new Headers(OriginalResponse.headers);
     const status = OriginalResponse.status;
 
-
-    if (!EnableCache) {
+    //如果用戶配置了强制禁用緩存則設置Cache-Control標頭為no-store。
+    if (DisableCache) {
         NewResponseHeaders.set('Cache-Control', 'no-store');
     }
+
     NewResponseHeaders.set("access-control-allow-origin", "*");
     NewResponseHeaders.set("access-control-allow-credentials", "true");
     NewResponseHeaders.delete("content-security-policy");
@@ -205,7 +232,6 @@ async function fetchAndApply(request) {
     const ContentType = NewResponseHeaders.get("content-type");
 
     let ReplacedText;
-
     if (ContentType !== null && ContentType !== undefined) {
         if (ContentType.includes('text/html') && ContentType.includes('UTF-8')) {
             ReplacedText = await OriginalResponse.text()
